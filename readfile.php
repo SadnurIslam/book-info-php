0ab8ebcda1789895711f52b225a0964ac4e583df
Sadnur Islam <sadnurislam2000@gmail.com>
2024-10-24 03:27:28 +0600

    crud

--- readfile.php
+++
@@ -1,5 +0,0 @@
-<?php 
-    $booksJson = file_get_contents('books.json');
-    $books = json_decode($booksJson, true);
-    $keys = array('title', 'author', 'available', 'pages', 'isbn');
-?>
--- updatebook.php
+++ updatebook.php
@@ -1,18 +1,7 @@
-<?php require 'readfile.php';
-    $title1 = $author1 = $available1 = $pages1 = $isbn1 = "";
-    $index = "";
-    if(isset($_POST['edit'])){
-        $index = $_POST['edit'];
-        $title1 = $books[$index]['title'];
-        $author1 = $books[$index]['author'];
-        $available1 = $books[$index]['available'];
-        $pages1 = $books[$index]['pages'];
-        $isbn1 = $books[$index]['isbn'];
-    }
-
+<?php 
+    require 'db.php';
 ?>
 
-<?php require 'readfile.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -52,6 +41,7 @@
     <h3 class="text-center">Edit Book Info</h3>
     <br>
     <form action="updatebook.php" method="post">
+        <input type="hidden" name="id" value="<?php if(isset($_POST['id'])) echo $_POST['id']; ?>">
         <table>
             <tbody>
                 <tr>
@@ -67,18 +57,12 @@
                 <tr>
                     <td><label for="available">Available: </label></td>
                     <td>
-                        <input type="radio" value="1" name="available">Yes
-                        <input type="radio" value="" name="available">No 
+                        <input type="number" name="available" id="available" value="<?php echo $available1;?>" required>
                     </td>
-                    <?php if($available1 == true): ?>
-                        <script>document.getElementsByName('available')[0].checked = true;</script>
-                    <?php else: ?>
-                        <script>document.getElementsByName('available')[1].checked = true;</script>
-                    <?php endif; ?>
                 </tr>
                 <tr>
                     <td><label for="pages">Pages: </label></td>
-                    <td><input class="form-control" type="text" id="pages" name="pages" required value="<?php echo $pages1;?>"></td>
+                    <td><input class="form-control" type="number" id="pages" name="pages" required value="<?php echo $pages1;?>"></td>
                 </tr>         
                 <tr>
                     <td><label for="isbn">isbn: </label></td>
@@ -86,41 +70,13 @@
                 </tr>    
                 <tr>
                     <td></td>
-                    <td style="text-align:center"><button id="back" class="btn btn-secondary">Cancel</button><input class="btn btn-success" type="submit" value="Update" name="submit" id="btn">
+                    <td style="text-align:center"><a href="index.php" class="btn btn-secondary">Cancel</a> <input class="btn btn-success" type="submit" value="Update" name="update" id="btn">
                     </td>
                 </tr>
-                <?php if($index != ""): ?>
-                    <input class="vis-hidden" type="text" name="index" value="<?php echo $index; ?>">
-                <?php endif; ?>   
             </tbody>
         </table>
     </form>
     </div>
-    <?php if(isset($_POST['submit'])){
-        $title = $_POST['title'];
-        $author = $_POST['author'];
-        $available = $_POST['available'];
-        if($available == 1){
-            $available = true;
-        }else{
-            $available = false;
-        }
-        $pages = $_POST['pages'];
-        $isbn = $_POST['isbn'];
-        $index = $_POST['index'];
-        $newBook = array('title' => $title, 'author' => $author, 'available' => $available, 'pages' => $pages, 'isbn' => $isbn);
-        $books[$index] = $newBook;
-        echo $index."<br>";
-        $booksJson = json_encode($books, JSON_PRETTY_PRINT);
-        file_put_contents('books.json', $booksJson);
-        echo "<script>window.location.href = 'editbook.php';</script>";
-    }
-    ?>
     <script src="assets/bootstrap5/js/bootstrap.min.js"></script>
-    <script>
-        document.getElementById('back').addEventListener('click', function(){
-            window.location.href = 'editbook.php';
-        });
-    </script>
 </body>
 </html>
